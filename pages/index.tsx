9b2ad2ccf796ed23d78774bd0b66491055181330
Thomzes <thomz161@gmail.com>
2022-10-15 18:10:05 +0700

    Refactor Login Navbar Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,6 @@ const Home: NextPage = () => {
   return (
     <>
       <Navbar />
-
       <section className="header pt-lg-60 pb-50">
         <div className="container-xxl container-fluid">
           <div className="row gap-lg-0 gap-5">
